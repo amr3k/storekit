3a5cade4b761bc2c3bd4fa4825bbf4f888ce899b
Amr <akkk33@protonmail.com>
2021-12-28 20:15:09 +0200

    Added escape key handler to close overlay

--- src/lib/Components/overlay.svelte
+++ src/lib/Components/overlay.svelte
@@ -6,6 +6,13 @@
 	const overlayClick = () => {
 		dispatch('overlayClick');
 	};
+	const handleKeyDown = (event: KeyboardEvent) => {
+		if (event.key === 'Escape') {
+			dispatch('overlayClick');
+		}
+	};
 </script>
+
+<svelte:window on:keydown={handleKeyDown} />
 
 <div class="fixed inset-0 h-full w-full" on:click={overlayClick} />
