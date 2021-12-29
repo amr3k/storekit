b297a41cd3de72993fc097ac8d64dec4b661b57a
Amr <akkk33@protonmail.com>
2021-12-29 16:37:40 +0200

    Using different cart icons based on cart content with notification indicator

---
+++ src/lib/Header/Sidebar/sidebar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const pages: { title: string; url: string }[] = [
+		{
+			title: 'Home',
+			url: '/'
+		},
+		{
+			title: 'Men',
+			url: '/category/men'
+		},
+		{
+			title: 'Women',
+			url: '/category/women'
+		},
+		{
+			title: 'Accessories',
+			url: '/category/accessories'
+		},
+		{
+			title: 'Outlet',
+			url: '/category/outlet'
+		}
+	];
+
+	const dispatch = createEventDispatcher();
+	const closeSideBar = (): void => {
+		dispatch('closeSideBar');
+	};
+</script>
+
+<!-- TODO convert hidden icons to appropriate elements [Language switcher => dropdown menu] -->
+
+<div class="drawer-side lg:hidden">
+	<label for="main-drawer" class="drawer-overlay" />
+	<ul class="p-4 overflow-y-auto w-80 bg-base-200">
+		{#each pages as page}
+			<li>
+				<a on:click={closeSideBar} href={page.url}>
+					{page.title}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style lang="postcss">
+	.drawer-side ul > li {
+		@apply mx-auto my-1 py-2 flex justify-items-stretch justify-center;
+	}
+	.drawer-side ul > li > a {
+		@apply w-full text-center;
+	}
+</style>
