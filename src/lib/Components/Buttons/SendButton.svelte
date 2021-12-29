c3e92c7096b2b15f68746979895503252189d9e4
Amr <akkk33@protonmail.com>
2021-12-29 19:12:15 +0200

    Using svelte actions instead of overlay

--- src/lib/Components/Buttons/SendButton.svelte
+++ src/lib/Components/Buttons/SendButton.svelte
@@ -8,7 +8,7 @@
 	<div class="svg-wrapper-1">
 		<div class="svg-wrapper">
 			<svg
-				class="block w-8 origin-center transition-transform duration-300 ease-in-out"
+				class="block w-8 mr-2 origin-center transition-transform duration-300 ease-in-out"
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
 				><path fill="none" d="M0 0h24v24H0z" /><path
@@ -27,11 +27,7 @@
 	}
 
 	button:hover svg {
-		@apply translate-x-7 rotate-45 scale-110;
-	}
-
-	button:hover span {
-		@apply translate-x-20;
+		@apply rotate-45 scale-110;
 	}
 
 	@keyframes fly-1 {
