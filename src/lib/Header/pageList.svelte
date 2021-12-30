24bf7b69a5ce47d502984b3c94105db2f940849b
Amr <akkk33@protonmail.com>
2021-12-30 02:59:27 +0200

    Using svelte context API instead of props Fixed bug with header pages menu

--- src/lib/Header/pageList.svelte
+++ src/lib/Header/pageList.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { Pages } from '$lib/Types/UI/header';
-	import { onMount } from 'svelte';
+	import { getContext } from 'svelte';
 
-	// $: currentPathname = window.location.pathname || '/';
-	export let pages: Pages;
-	let currentPathname = '/';
-	onMount(() => {
-		currentPathname = window.location.pathname;
-	});
-	const updateCurrentPathname = (pathname: string) => {
-		currentPathname = pathname;
-	};
+	let pages: Pages = getContext('pages');
+	$: currentPathname = $page.path;
 </script>
 
 <div class="flex items-stretch">
@@ -18,20 +12,14 @@
 		<a
 			href={page.url}
 			class:selected={page.url === currentPathname}
-			class="navbar-category"
-			on:click={() => updateCurrentPathname(page.url)}>{page.title}</a
+			class="mx-3 my-1 font-medium leading-6 border-b border-b-transparent hover:border-b-primary hover:duration-500"
+			>{page.title}</a
 		>
 	{/each}
 </div>
 
 <style lang="postcss">
-	.navbar-category {
-		@apply mx-3 my-1 font-medium leading-6 border-b border-b-transparent;
-	}
-	.navbar-category.selected {
+	.selected {
 		@apply border-b-secondary;
-	}
-	.navbar-category:hover {
-		@apply border-b border-b-primary duration-500;
 	}
 </style>
