b64c8ea476828f15f08501033e04134b10701aa3
Amr <akkk33@protonmail.com>
2021-12-31 23:32:30 +0200

    Bug fix in error fallback Initialized basic hooks Initialized account endpoint Testing layout reset

---
+++ src/hooks/options.ts
@@ -0,0 +1,12 @@
+import { setLanguage } from '$lib/Stores/language';
+import * as cookieLib from 'cookie';
+
+export const userLanguage = async (httpCookieHeader) => {
+  // This function checks request cookies,
+  // and adjust local storage and store after it.
+  const cookieObject = cookieLib.parse(httpCookieHeader || '');
+
+  setLanguage(cookieObject.language || 'en');
+
+  return cookieObject.language || 'en';
+}
---
+++ src/routes/account/__layout.reset.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	// This file is created so that /admin/* pages do not inherit the root layout
+	// Layout resets are identical to normal layout components.
+</script>
+
+<slot />
