8426b2837b10000cfa14927d1791f7d2a58c454e
Amr <akkk33@protonmail.com>
2022-01-02 05:10:49 +0200

    Moving all components to $lib/Components Added blank structure to categories starting with men

---
+++ src/lib/Components/Category/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// The sidebar should make use of product filters
+	// The sidebar should only appear on large screens <lg>
+	// For small and medium screens, a filter icon should appear
+	// The filter icon should be clickable and toggle an overlay with filters
+	// More like the search overlay
+</script>
+
+<aside>
+	<!-- TBI -->
+</aside>
---
+++ src/routes/category/men.svelte
@@ -0,0 +1,7 @@
+<script context="module">
+	// Call the ts endpoint to retrieve products
+</script>
+
+<script lang="ts">
+	//
+</script>
