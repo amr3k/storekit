fa827d7be2b454cd03ad3afd0e53cd3353871126
Amr <akkk33@protonmail.com>
2022-01-08 14:11:14 +0200

    Changing lazyload mechanism: Using svelte await instead of action. Known bug: It stops working when changing endpoint. Fixing in the next update

--- src/lib/Components/Media/image.svelte
+++ src/lib/Components/Media/image.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-	import { lazyLoad } from '$lib/Actions/lazyload';
-	export let classes: string;
-	export let src: string;
-	export let alt: string;
+	export let classes: string; // Tailwind classes
+	export let source: string; // Image source
+	export let alt: string; // Image alternative title
+	export let aspectRation: string = '2/3'; // Image aspect ratio, this will be used for the placeholder
+
+	const preload = async (): Promise<string> => {
+		/**
+		 * Preload image and return as base64
+		 */
+		let res: Response = await fetch(source);
+		let blob: Blob = await res.blob();
+		let reader: FileReader = new FileReader();
+		return new Promise(function (resolve) {
+			reader.readAsDataURL(blob);
+			reader.onload = () => resolve(reader.result);
+			reader.onerror = (error) => console.error('Error: ', error);
+		});
+	};
 </script>
 
-<!-- Using 1x1 pixel placeholder -->
-<img
-	class={classes}
-	use:lazyLoad={src}
-	src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEXV1dUmzt0AAAAACklEQVQI12NgAAAAAgAB4iG8MwAAAABJRU5ErkJggg=="
-	loading="lazy"
-	width="100%"
-	height="100%"
-	{alt}
-/>
+{#await preload()}
+	<!-- Display a simple fading gray placeholder to preserve the space while the image is loading -->
+	<div class="aspect-[{aspectRation}] animate-pulse bg-base-300 " {alt} />
+{:then base64}
+	<img class={classes} src={base64} loading="lazy" width="100%" height="100%" {alt} />
+{:catch}
+	<!-- In case of an error, keep the placeholder -->
+	<div class="aspect-[{aspectRation}] bg-base-300 " {alt} />
+{/await}
