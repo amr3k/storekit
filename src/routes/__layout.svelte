a74403f408fd5511b363ae591afa4dc7b86bcaad
Amr <akkk33@protonmail.com>
2022-01-08 02:07:48 +0200

    Moving global tailwind CSS to layout Using tailwind's bounce animation instead of the custom one

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,3 +11,31 @@
 <Header>
 	<slot />
 </Header>
+
+<style global lang="postcss">
+	/* Write your global styles here, in PostCSS syntax */
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	html {
+		@apply antialiased;
+	}
+	::selection {
+		@apply bg-neutral text-neutral-content;
+	}
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-family: 'Cairo', sans-serif;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,16 +6,6 @@ const config = {
 			fontFamily: {
 				sans: ['Open Sans', 'sans-serif']
 			},
-			keyframes: {
-				'fly-1': {
-					from: {
-						transform: 'translateY(0.1em)'
-					},
-					to: {
-						transform: 'translateY(-0.1em)'
-					}
-				}
-			}
 		}
 	},
 
