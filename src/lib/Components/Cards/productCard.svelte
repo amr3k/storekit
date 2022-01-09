09e195e5d92d01d37f2cf9427d029a1a16c9c127
Amr <akkk33@protonmail.com>
2022-01-09 09:08:15 +0200

    Added product sale badge (with RTL support)

--- src/lib/Components/Cards/productCard.svelte
+++ src/lib/Components/Cards/productCard.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
-	import { preloadImage } from '$lib/Actions/preloadImage';
+	import { preloadImage } from '$lib/Functions/preloadImage';
+	import { language as languageStore } from '$lib/Stores/language';
 	import type { Product } from '$lib/Types/Data/product.types';
+	import { roundPrice } from '$lib/Functions/roundPrice';
 	export let product: Product;
 	const productInStock = product.stock_status === 'instock';
+	// console.log(product);
 </script>
 
 {#await preloadImage(product.images[0].src)}
-	<div class="w-full h-full aspect-[2/3] bg-base-300 animate-pulse rounded-lg border border-gray-100" />
+	<div
+		class="w-full h-full aspect-[2/3] bg-base-300 animate-pulse rounded-lg border border-gray-100"
+	/>
 {:then base64}
 	<div
 		in:fade={{ duration: 500 }}
-		class="group w-full bg-base-200 rounded-lg flex flex-col items-center overflow-hidden hover:shadow-lg border border-gray-100"
+		class="group w-full bg-base-200 relative rounded-lg flex flex-col overflow-hidden hover:shadow-lg border border-gray-100"
 	>
 		<a sveltekit:prefetch href="/product/{product.slug}" class="w-full h-full relative">
 			<img class="object-cover w-full h-full" src={base64} alt={product.images[0].alt} />
@@ -21,15 +26,24 @@
 				alt={product.images[1].alt}
 			/>
 		</a>
-		<div class="px-4 py-2">
-			<h3 class="font-medium">
+		<!-- {#if product.on_sale} -->
+		<div
+			class="absolute bg-red-500 text-white select-none px-2 py-1 top-0 {$languageStore === 'en'
+				? 'right-0 rounded-tr-lg rounded-bl-lg'
+				: 'left-0 rounded-tl-lg rounded-br-lg'}"
+		>
+			Sale
+		</div>
+		<!-- {/if} -->
+		<div class="p-2">
+			<h3 class="font-medium text-lg truncate">
 				<a href="/product/{product.slug}" title={product.name}>
 					{product.name}
 				</a>
 			</h3>
-			<p class="">{product.price}</p>
-			<p class="">{product.regular_price}</p>
-			<p class="">{product.sale_price}</p>
+			<p class="">{roundPrice(parseFloat(product.price))}</p>
+			<!-- <p class="">{product.regular_price}</p>
+			<p class="">{product.sale_price}</p> -->
 			<!-- <p>{product.stock_quantity}</p> -->
 		</div>
 	</div>
