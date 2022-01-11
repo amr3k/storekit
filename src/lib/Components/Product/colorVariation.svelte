2173427ac32082a792b553fd3898b5a5711055c5
Amr <akkk33@protonmail.com>
2022-01-11 16:49:43 +0200

    Added basic color variation switch (WiP)

--- src/lib/Components/Product/colorVariation.svelte
+++ src/lib/Components/Product/colorVariation.svelte
@@ -1,23 +1,38 @@
 <script lang="ts">
 	// let variationLink: string;
+	export let choice: string;
+	// TODO the choice binding doesn't work, it doesn't switch current color
 	export let colorName: string;
 	const preloadColor = async (): Promise<string> => {
 		const response: Response = await fetch(`/api/preloadColor?colorName=${colorName}`);
 		const data = await response.json();
 		return data.hexCode;
 	};
+	const randomNumber: number = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
 </script>
 
 {#await preloadColor()}
 	<div class="mr-1 rounded-full bg-base-300 animate-pulse w-6 h-6" />
 {:then backgroundColor}
-	<div
-		title={colorName}
-		style="background-color: {backgroundColor};"
-		class="mr-1 rounded-full w-6 h-6 flex justify-center items-center"
-	>
-		<div
-			class="w-5 h-5 rounded-full border-2 border-transparent hover:border-base-200 duration-200 ease-linear"
+	<input
+		bind:group={colorName}
+		class="peer absolute left-[-9999px]"
+		id="product-color-{colorName}-{randomNumber}"
+		type="radio"
+		name="product-color"
+		value={colorName}
+	/>
+	<div class="relative inline-block w-8 h-8 hover:shadow-md rounded-full gradient-border">
+		<label
+			style="background-color: {backgroundColor}"
+			class="absolute inset-0 w-7 h-7 m-auto rounded-full cursor-pointer {choice === colorName
+				? 'border-2 border-double border-white'
+				: ''}"
+			for="product-color-{colorName}-{randomNumber}"
+			aria-label={colorName}
+			aria-checked={choice === colorName}
+			aria-roledescription="Product color"
+			aria-details={colorName}
 		/>
 	</div>
 {/await}
