d427dac3513fc71c8e0f40ee37c1c3bfaec9047f
Amr <akkk33@protonmail.com>
2022-01-24 22:58:15 +0200

    Upgraded deps Updated deprecated code Using yarn instead of npm

--- src/routes/api/product/variable.ts
+++ src/routes/api/product/variable.ts
@@ -1,8 +1,7 @@
-import type { Request } from '@sveltejs/kit';
 import { WOO_AUTH_HEADER } from '$lib/config';
 
-export const get = async (req: Request) => {
-	const productID = req.params.id;
+export const get = async ({ params }) => {
+	const productID = params.id;
 	const _url = import.meta.env.VITE_WOO_ENDPOINT + `/products?${productID}/variations`;
 	const res: Response = await fetch(_url, {
 		method: 'GET',
