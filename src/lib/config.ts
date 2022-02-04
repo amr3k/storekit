099dbf6f3f86c7d662fa9f7d12861324684c93c1
Amr <akkk33@protonmail.com>
2022-02-04 15:54:49 +0200

    Modifying pre-commit script to add the newly modified files by formatter

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,6 @@
 import { encode } from 'js-base64';
 
-import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-
+import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 export const WOO_ENDPOINT = process.env.WOO_ENDPOINT;
 export const WOO_AUTH_HEADER =
